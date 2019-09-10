6c28cf2cd6efc5b966ca14106be570e139a11ee4
Bryan Hou <boyuhou@gmail.com>
2019-09-10 17:16:32 -0400

    update indicators

--- DailyReport/__init__.py
+++ DailyReport/__init__.py
@@ -1,3 +1,4 @@
+import os
 from docxtpl import DocxTemplate, InlineImage, RichText
 from docx.shared import Mm
 from decimal import Decimal, ROUND_HALF_UP
@@ -6,7 +7,6 @@ import pandas as pd
 import numpy as np
 from PIL import Image
 import comtypes.client
-
 
 WORD2PDF_FORMAT = 17
 
@@ -24,21 +24,74 @@ DICT_KEY_ALL = 'all'
 DASH_CELL_VALUE = '-'
 COLOUR_DEFAULT_FG = COLOURS.COLOUR_BLACK
 COLOUR_DEFAULT_BG = COLOURS.COLOUR_WHITE
+COLOUR_DEFAULT_HIGHLIGHT = COLOURS.COLOUR_ORANGE_YELLOW
+
+LARGE_CELL_FONT_SIZE = 21
+LARGE_ROW_LABEL_FONT_SIZE = 22
+LARGE_COL_LABEL_FONT_SIZE = 22
+
+MEDIUM_CELL_FONT_SIZE = 16
+MEDIUM_ROW_LABEL_FONT_SIZE = 16
+MEDIUM_COL_LABEL_FONT_SIZE = 16
+
+SMALL_CELL_FONT_SIZE = 12
+SMALL_ROW_LABEL_FONT_SIZE = 12
+SMALL_COL_LABEL_FONT_SIZE = 12
 
 
-def combine_two_image(image_list, target_image_path):
-    images = map(Image.open, image_list)
-    widths, heights = zip(*(i.size for i in images))
-    total_width = sum(widths)
-    max_height = max(heights)
-    new_im = Image.new('RGB', (total_width, max_height))
+def cell_bg_shade(row_group, col_group, cell, highlight_thresholds, cur_row):
+    if row_group is None or col_group is None:
+        return COLOUR_DEFAULT_BG
+    elif row_group == col_group:
+        return COLOURS.COLOUR_LIGHT_GRAY
+    else:
+        return COLOUR_DEFAULT_BG
 
-    x_offset = 0
-    for im in images:
-        new_im.paste(im, (x_offset, 0))
-        x_offset += im.size[0]
 
-    new_im.save(target_image_path)
+def cell_bg_asset_threshold_highlight_or_shade(row_group, col_group, cell, highlight_thresholds, cur_row):
+    if not highlight_thresholds:
+        return cell_bg_shade(row_group, col_group, cell, highlight_thresholds, cur_row)
+
+    if row_group in highlight_thresholds and highlight_thresholds[row_group] <= abs(cell):
+        return COLOUR_DEFAULT_HIGHLIGHT
+
+    if col_group in highlight_thresholds and highlight_thresholds[col_group] <= abs(cell):
+        return COLOUR_DEFAULT_HIGHLIGHT
+
+    return cell_bg_shade(row_group, col_group, cell, highlight_thresholds, cur_row)
+
+
+def cell_bg_every_other_row(row_group, col_group, cell, highlight_thresholds, cur_row):
+    if cur_row % 2 == 0:
+        return COLOURS.COLOUR_LIGHT_GRAY
+
+
+def cell_fg_colour_by_column_name(column_name, colour_dict):
+    return colour_dict.get(column_name, None)
+
+
+DEFAULT_GRIDS_CELL_FORMAT_CONFIG = {
+    'default': {
+        'cell_font_size': MEDIUM_CELL_FONT_SIZE,
+        'row_label_font_size': MEDIUM_ROW_LABEL_FONT_SIZE,
+        'col_label_font_size': MEDIUM_COL_LABEL_FONT_SIZE,
+        'make_labels_bold': False,
+        'format_num_decimal': 2,
+        'background_colour_ftn': cell_bg_every_other_row,
+        'highlight_thresholds': None
+    }
+}
+
+
+def combine_images(list_im, target_path):
+    imgs = [Image.open(i) for i in list_im]
+    # pick the image which is the smallest, and resize the others to match it (can be arbitrary image shape here)
+    min_shape = sorted([(np.sum(i.size), i.size) for i in imgs])[0][1]
+    imgs_comb = np.hstack([np.asarray(i.resize(min_shape)) for i in imgs])
+
+    # save that beautiful picture
+    imgs_comb = Image.fromarray(imgs_comb)
+    imgs_comb.save(target_path)
 
 
 def save_doc(context, inline_imgs, file_dict, template_path, result_path):
@@ -74,6 +127,36 @@ def doc2pdf(input_path, output_path):
             doc.Close()
     finally:
         word.Quit()
+
+
+def get_group_value(group_path, instrument_list, indicator_name):
+    result = {}
+    for i in instrument_list:
+        _path = os.path.join(group_path, i + '.csv')
+        _df = pd.read_csv(_path, header=None)
+        result[i] = _df.iloc[-1, -1]
+    return pd.DataFrame(result, [indicator_name])
+
+
+def get_specific_value(group_path, instrument, column_name):
+    _path = os.path.join(group_path, instrument + '.csv')
+    _df = pd.read_csv(_path)
+    return _df.loc[:, column_name].iloc[-1]
+
+
+def get_value_area_style(val, vah, vpoc, atr):
+    style = ''
+    if vah-val >= 0.5 * atr:
+        style = style + 'Wide, '
+    if vah - vpoc <= 0.1 * atr:
+        style = style + 'Stacked Up, '
+    if vpoc - val <= 0.1 * atr:
+        style = style + 'Stacked Bottom, '
+    if vah - val <= 0.25 * atr:
+        style = style + 'Tight, '
+    if style == '':
+        style = 'Normal, '
+    return style[:-2]
 
 
 class GridBuilder:
@@ -156,6 +239,7 @@ class GridBuilder:
     """
     Given a row in df, return the corresponding RichText object for each cell
     """
+
     @staticmethod
     def __get_column_structure(df_row, cell_format_config, cur_row, row_group=None, col_group=None):
         result_cols = []
@@ -176,7 +260,7 @@ class GridBuilder:
                                                                          )
             text_colour = None
             if cell_config.get('text_colour_ftn', None):
-                text_colour = cell_config['text_colour_ftn'](column_labels[i],cell_config['text_colour_dict'])
+                text_colour = cell_config['text_colour_ftn'](column_labels[i], cell_config['text_colour_dict'])
             result_cols.append(
                 GridBuilder.__get_cell_info_dict(
                     cell=columns[i],
@@ -217,19 +301,24 @@ class GridBuilder:
                 cell_section_config = cell_format_config[
                     CELL_FORMAT_DEFAULT_KEY] if cur_section not in cell_format_config else cell_format_config[
                     cur_section]
-                col_dict[cur_section] = GridBuilder.__get_column_structure(GridBuilder.__get_df_col_segment(df, cur_section), cell_section_config,
-                                                                    cur_row, row_group, cur_section)
+                col_dict[cur_section] = GridBuilder.__get_column_structure(
+                    GridBuilder.__get_df_col_segment(df, cur_section), cell_section_config,
+                    cur_row, row_group, cur_section)
 
             col_dict[DICT_KEY_ALL] = GridBuilder.__merge_formatted_multiindex_df_into_one(df, col_dict, is_row=False)
         else:
             if len(cell_format_config) > 1:
                 for column in df.columns:
-                    cell_section_config = cell_format_config[CELL_FORMAT_DEFAULT_KEY] if column not in cell_format_config else cell_format_config[
+                    cell_section_config = cell_format_config[
+                        CELL_FORMAT_DEFAULT_KEY] if column not in cell_format_config else cell_format_config[
                         column]
                     col_dict[column] = GridBuilder.__get_column_structure(df[[column]], cell_section_config, cur_row)
-                col_dict[DICT_KEY_ALL] = GridBuilder.__merge_formatted_multiindex_df_into_one(df, col_dict, is_row=False)
+                col_dict[DICT_KEY_ALL] = GridBuilder.__merge_formatted_multiindex_df_into_one(df, col_dict,
+                                                                                              is_row=False)
             else:
-                col_dict[DICT_KEY_ALL] = GridBuilder.__get_column_structure(df, cell_format_config[CELL_FORMAT_DEFAULT_KEY], cur_row)
+                col_dict[DICT_KEY_ALL] = GridBuilder.__get_column_structure(df,
+                                                                            cell_format_config[CELL_FORMAT_DEFAULT_KEY],
+                                                                            cur_row)
 
         return col_dict
 
@@ -239,13 +328,18 @@ class GridBuilder:
         row_id = 0
         for cur_row_data in df.index:
             row_id = row_id + 1
-            col_dict = GridBuilder.__get_column_structure_by_section(GridBuilder.__get_df_row_segment(df, cur_row_data), cell_format_config, row_id, row_group)
+            col_dict = GridBuilder.__get_column_structure_by_section(GridBuilder.__get_df_row_segment(df, cur_row_data),
+                                                                     cell_format_config, row_id, row_group)
             col_dict['label'] = RichText(cur_row_data,
-                                         bold=cell_format_config[CELL_FORMAT_DEFAULT_KEY].get('make_labels_bold', False),
+                                         bold=cell_format_config[CELL_FORMAT_DEFAULT_KEY].get('make_labels_bold',
+                                                                                              False),
                                          size=cell_format_config[CELL_FORMAT_DEFAULT_KEY].get('row_label_font_size', 8))
             # if background colour is specified:
-            if (CELL_FORMAT_DEFAULT_KEY in cell_format_config) & ('background_colour_ftn' in cell_format_config[CELL_FORMAT_DEFAULT_KEY]):
-                background_colour = cell_format_config[CELL_FORMAT_DEFAULT_KEY]['background_colour_ftn'](None, None, None, None, row_id)
+            if (CELL_FORMAT_DEFAULT_KEY in cell_format_config) & (
+                    'background_colour_ftn' in cell_format_config[CELL_FORMAT_DEFAULT_KEY]):
+                background_colour = cell_format_config[CELL_FORMAT_DEFAULT_KEY]['background_colour_ftn'](None, None,
+                                                                                                         None, None,
+                                                                                                         row_id)
                 col_dict['bg'] = background_colour
 
             row_list.append(col_dict)
@@ -263,8 +357,9 @@ class GridBuilder:
             row_level_sections = df.index.get_level_values(0).unique().tolist()
 
             for cur_section in row_level_sections:
-                data_dict[cur_section] = GridBuilder.__get_row_structure(GridBuilder.__get_df_row_segment(df, cur_section), cell_format_config,
-                                                                         cur_section)
+                data_dict[cur_section] = GridBuilder.__get_row_structure(
+                    GridBuilder.__get_df_row_segment(df, cur_section), cell_format_config,
+                    cur_section)
 
             data_dict[DICT_KEY_ALL] = GridBuilder.__merge_formatted_multiindex_df_into_one(df, data_dict, is_row=True)
         else:
@@ -276,11 +371,12 @@ class GridBuilder:
             column_level_sections = df.columns.get_level_values(0).unique()
 
             for cur_section in column_level_sections:
-                column_dict[cur_section] = GridBuilder.__get_rich_text_columns(GridBuilder.__get_df_col_segment(df, cur_section).columns.tolist(),
-                                                                               cell_format_config)
+                column_dict[cur_section] = GridBuilder.__get_rich_text_columns(
+                    GridBuilder.__get_df_col_segment(df, cur_section).columns.tolist(),
+                    cell_format_config)
 
                 column_dict[DICT_KEY_ALL] = GridBuilder.__get_rich_text_columns(df.columns.get_level_values(1).tolist(),
-                                                                                     cell_format_config)
+                                                                                cell_format_config)
         else:
             column_dict[DICT_KEY_ALL] = GridBuilder.__get_rich_text_columns(df.columns.tolist(), cell_format_config)
 
