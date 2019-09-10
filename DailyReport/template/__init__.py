e9cff89d0deb37d979cd75e92fd3ebadb8e722b5
Bryan Hou <boyuhou@gmail.com>
2019-09-09 23:45:30 -0400

    add initial data handling

---
+++ DailyReport/template/__init__.py
@@ -0,0 +1,5 @@
+import os
+
+
+WEEKLY_TEMPLATE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'WEEKLY_TEMPLATE.docx')
+DAILY_TEMPLATE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'DAILY_TEMPLATE.docx')
