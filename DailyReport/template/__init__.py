c6f0fa97ab38c86d727fdfaf67d7c84d232ca2ed
Bryan Hou <boyuhou@gmail.com>
2019-10-15 02:01:07 -0400

    add daily and weekly charts

--- DailyReport/template/__init__.py
+++ DailyReport/template/__init__.py
@@ -3,3 +3,5 @@ import os
 
 WEEKLY_TEMPLATE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'WEEKLY_TEMPLATE.docx')
 DAILY_TEMPLATE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'DAILY_TEMPLATE.docx')
+DAILY_CHART_TEMPLATE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'DAILY_CHART_TEMPLATE.docx')
+WEEKLY_CHART_TEMPLATE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'WEEKLY_CHART_TEMPLATE.docx')
