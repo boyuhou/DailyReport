226abfb4721de625b5825a955319c792ad5280fb
Bryan Hou <boyuhou@gmail.com>
2019-10-16 00:53:14 -0400

    report update

--- DailyReport/template/__init__.py
+++ DailyReport/template/__init__.py
@@ -1,7 +1,8 @@
 import os
 
-
-WEEKLY_TEMPLATE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'WEEKLY_TEMPLATE.docx')
-DAILY_TEMPLATE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'DAILY_TEMPLATE.docx')
-DAILY_CHART_TEMPLATE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'DAILY_CHART_TEMPLATE.docx')
+WEEKLY_CHART_HEADER_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'WEEKLY_CHART_HEADER.docx')
 WEEKLY_CHART_TEMPLATE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'WEEKLY_CHART_TEMPLATE.docx')
+WEEKLY_TEMPLATE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'WEEKLY_TEMPLATE.docx')
+DAILY_CHART_HEADER_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'DAILY_CHART_HEADER.docx')
+DAILY_CHART_TEMPLATE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'DAILY_CHART_TEMPLATE.docx')
+DAILY_TEMPLATE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'DAILY_TEMPLATE.docx')
